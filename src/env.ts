1e5241c4e4e704978dcdbef1ac343dc95be02bb4
Bennie Rosas <bennie.rosas@blvd.co>
2024-06-22 20:48:34 -0500

    install packages; clean up npm scripts; update dotenv usage

--- src/env.ts
+++ src/env.ts
@@ -1,20 +1,43 @@
-import dotenv from 'dotenv'
+// https://github.com/keithmorris/node-dotenv-extended
+import type { AppEnv } from './types'
+import dotenvExtended from 'dotenv-extended'
 
-dotenv.config({
-  path:
-    process.env.POLIS_ENV === 'prod'
-      ? 'prod.env'
-      : process.env.POLIS_ENV === 'preprod'
-        ? 'preprod.env'
-        : '.env'
+// Determine the environment (e.g. dev, preprod, prod)
+const envName = process.env.POLIS_ENV || 'dev'
+// Use .env file for dev, otherwise use `${envName}.env`
+const envPath = envName === 'dev' ? '.env' : `${envName}.env`
+
+// Load the base .env file and overlay the environment-specific file, if it exists
+dotenvExtended.load({
+  path: envPath,
+  defaults: '.env.defaults',
+  schema: '.env.schema',
+  errorOnMissing: true,
+  errorOnExtra: true,
+  errorOnRegex: true
 })
 
-export default {
-  dbDatabase: process.env.DB_DATABASE || 'politeia-dev',
-  dbHost: process.env.DB_HOST || 'localhost',
-  dbLogging: process.env.DB_LOGGING === 'true',
-  dbPassword: process.env.DB_PASSWORD || 'postgres',
-  dbPort: parseInt(process.env.DB_PORT || '5432', 10),
-  dbSynchronize: process.env.NODE_ENV !== 'production',
-  dbUsername: process.env.DB_USERNAME || 'postgres'
+function asBoolean(value: string | undefined): boolean {
+  return value?.toLowerCase() === 'true'
 }
+
+function asInteger(value: string | undefined): number | undefined {
+  if (typeof value === 'string') {
+    return parseInt(value)
+  }
+}
+
+// TODO - Allow for more nuanced appLogging and dbLogging configurations.
+
+export default {
+  appHost: process.env.APP_HOST,
+  appLogging: asBoolean(process.env.APP_LOGGING),
+  appPort: asInteger(process.env.APP_PORT),
+  dbDatabase: process.env.DB_DATABASE,
+  dbHost: process.env.DB_HOST,
+  dbLogging: asBoolean(process.env.DB_LOGGING),
+  dbPassword: process.env.DB_PASSWORD,
+  dbPort: asInteger(process.env.DB_PORT),
+  dbUsername: process.env.DB_USERNAME,
+  googleClientId: process.env.GOOGLE_CLIENT_ID
+} as AppEnv
---
+++ src/types.ts
@@ -0,0 +1,12 @@
+export type AppEnv = {
+  appHost?: string
+  appLogging?: boolean
+  appPort?: number
+  dbDatabase?: string
+  dbHost?: string
+  dbLogging?: boolean
+  dbPassword?: string
+  dbPort?: number
+  dbUsername?: string
+  googleClientId?: string
+}
