a06ea07276b64ac25a6916c2698826c5c05079a2
Bennie Rosas <bennie.rosas@blvd.co>
2024-06-22 20:55:27 -0500

    initialize fastify server

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,29 @@
-import { AppDataSource } from './data-source'
-import './env'
+import type { FastifyListenOptions } from 'fastify'
+import { AppDataSource } from './config/database'
+import fastify from 'fastify'
+import env from './env'
+import userRoutes from './routes/userRoutes'
 
-AppDataSource.initialize().catch((error) => console.log(error))
+export const startServer = async () => {
+  const app = fastify({ logger: env.appLogging })
+
+  await app.register(userRoutes, { prefix: '/users' })
+
+  const listenOptions: FastifyListenOptions = {
+    host: env.appHost,
+    port: env.appPort
+  }
+
+  try {
+    await AppDataSource.initialize()
+    console.log('Data Source has been initialized')
+    await app.listen(listenOptions)
+    console.log(`Server listening at ${env.appHost} on port ${env.appPort}`)
+  } catch (err) {
+    console.error('Error initializing data source: ', err)
+    app.log.error(err)
+    process.exit(1)
+  }
+}
+
+void startServer()
