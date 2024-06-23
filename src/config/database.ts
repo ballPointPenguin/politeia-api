dbb9f20d409b22021b0f2393c5ae134a88427205
Bennie Rosas <bennie.rosas@blvd.co>
2024-06-22 20:54:14 -0500

    move and rewrite data-source -> config/database

---
+++ src/config/database.ts
@@ -0,0 +1,21 @@
+// Required for TypeScript to work effectively with class decorators:
+import 'reflect-metadata'
+
+import type { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions'
+import { DataSource } from 'typeorm'
+import { User } from '../entities/User'
+import env from '../env'
+
+const options: PostgresConnectionOptions = {
+  type: 'postgres',
+  host: env.dbHost,
+  port: env.dbPort,
+  username: env.dbUsername,
+  password: env.dbPassword,
+  database: env.dbDatabase,
+  synchronize: false,
+  logging: env.dbLogging,
+  entities: [User]
+}
+
+export const AppDataSource = new DataSource(options)
