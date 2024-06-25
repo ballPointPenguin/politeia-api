0ea37fd1e3fbd2f823daa558e0e949f9da861dbc
Bennie Rosas <bennie.rosas@blvd.co>
2024-06-25 02:16:12 -0500

    handle request queries and pagination on User

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -2,14 +2,48 @@ import type { FastifyRequest, FastifyReply } from 'fastify'
 import { AppDataSource } from '../config/database'
 import { User } from '../entities/User'
 
+type RequestQuery = {
+  page?: number
+  limit?: number
+  sort?: string
+  order?: 'ASC' | 'DESC'
+  [key: string]: string | number | undefined // filters
+}
+
 export const listUsers = async (
   request: FastifyRequest,
   reply: FastifyReply
 ) => {
-  const userRepo = AppDataSource.getRepository(User)
-  const users = await userRepo.find()
+  const {
+    page = 1,
+    limit = 50,
+    sort = 'uid',
+    order = 'ASC'
+    // ...filters
+  } = request.query as RequestQuery
 
-  return reply.send(users)
+  const userRepo = AppDataSource.getRepository(User)
+  const alias = userRepo.metadata.targetName
+  const skip = (page - 1) * limit
+  const queryBuilder = userRepo.createQueryBuilder(alias)
+
+  // Apply filters (TODO)
+
+  // Apply sorting
+  queryBuilder.orderBy(`${alias}.${sort}`, order)
+
+  // Apply pagination
+  queryBuilder.skip(skip).take(limit)
+
+  const [users, total] = await queryBuilder.getManyAndCount()
+
+  return reply.send({
+    data: users,
+    total,
+    page,
+    limit,
+    totalPages: Math.ceil(total / limit)
+  })
 }
 
 export const getUser = async (request: FastifyRequest, reply: FastifyReply) => {
