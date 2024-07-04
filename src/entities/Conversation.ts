e66ef92a9f7cbcdb665b3b046493842b13ea3105
Bennie Rosas <bennie.rosas@blvd.co>
2024-07-04 18:38:31 -0500

    updated entities

--- src/entities/Conversation.ts
+++ src/entities/Conversation.ts
@@ -1,37 +1,31 @@
-import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm'
+import {
+  Column,
+  Entity,
+  Index,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn
+} from 'typeorm'
+// import { Course } from './Course'
+import { User } from './User'
 
 @Entity('conversations')
+@Index('conversations_owner_idx', ['owner'])
+@Index('conversations_zid_index', ['zid'], { unique: true })
+@Index('conversations_zid_key', ['zid'], { unique: true })
 export class Conversation {
-  @PrimaryGeneratedColumn({ name: 'zid' })
+  @PrimaryGeneratedColumn()
   zid!: number
 
-  @Column({ name: 'topic', type: 'varchar', length: 1000, nullable: true })
+  @Column({ type: 'varchar', length: 1000, nullable: true })
   topic!: string
 
   @Column({
-    name: 'description',
     type: 'varchar',
     length: 50000,
     nullable: true
   })
   description!: string
-
-  @Column({ name: 'link_url', type: 'varchar', length: 9999, nullable: true })
-  linkUrl!: string
-
-  @Column({ name: 'parent_url', type: 'varchar', length: 9999, nullable: true })
-  parentUrl!: string
-
-  @Column({ name: 'upvotes', type: 'integer', default: 1, nullable: false })
-  upvotes!: number
-
-  @Column({
-    name: 'participant_count',
-    type: 'integer',
-    default: 0,
-    nullable: true
-  })
-  participantCount!: number
 
   @Column({ name: 'is_anon', type: 'boolean', default: true, nullable: true })
   isAnon!: boolean
@@ -51,12 +45,31 @@ export class Conversation {
   isPublic!: boolean
 
   @Column({
-    name: 'is_data_open',
+    name: 'email_domain',
+    type: 'varchar',
+    length: 200,
+    nullable: true
+  })
+  emailDomain!: string
+
+  @Column({ type: 'integer', nullable: true })
+  owner!: number
+
+  @Column({
+    name: 'participant_count',
+    type: 'integer',
+    default: 0,
+    nullable: true
+  })
+  participantCount!: number
+
+  @Column({
+    name: 'strict_moderation',
     type: 'boolean',
     default: false,
     nullable: true
   })
-  isDataOpen!: boolean
+  strictModeration!: boolean
 
   @Column({
     name: 'profanity_filter',
@@ -74,102 +87,8 @@ export class Conversation {
   })
   spamFilter!: boolean
 
-  @Column({
-    name: 'strict_moderation',
-    type: 'boolean',
-    default: false,
-    nullable: true
-  })
-  strictModeration!: boolean
-
-  @Column({
-    name: 'prioritize_seed',
-    type: 'boolean',
-    default: false,
-    nullable: true
-  })
-  prioritizeSeed!: boolean
-
-  @Column({ name: 'vis_type', type: 'integer', default: 0, nullable: false })
-  visType!: number
-
-  @Column({ name: 'write_type', type: 'integer', default: 1, nullable: false })
-  writeType!: number
-
-  @Column({ name: 'help_type', type: 'integer', default: 1, nullable: false })
-  helpType!: number
-
-  @Column({
-    name: 'write_hint_type',
-    type: 'integer',
-    default: 1,
-    nullable: false
-  })
-  writeHintType!: number
-
-  @Column({ name: 'style_btn', type: 'varchar', length: 500, nullable: true })
-  styleBtn!: string
-
-  @Column({
-    name: 'socialbtn_type',
-    type: 'integer',
-    default: 0,
-    nullable: false
-  })
-  socialbtnType!: number
-
-  @Column({
-    name: 'subscribe_type',
-    type: 'integer',
-    default: 1,
-    nullable: false
-  })
-  subscribeType!: number
-
-  @Column({
-    name: 'branding_type',
-    type: 'integer',
-    default: 1,
-    nullable: false
-  })
-  brandingType!: number
-
-  @Column({ name: 'bgcolor', type: 'varchar', length: 20, nullable: true })
-  bgcolor!: string
-
-  @Column({ name: 'help_bgcolor', type: 'varchar', length: 20, nullable: true })
-  helpBgcolor!: string
-
-  @Column({ name: 'help_color', type: 'varchar', length: 20, nullable: true })
-  helpColor!: string
-
-  @Column({
-    name: 'email_domain',
-    type: 'varchar',
-    length: 200,
-    nullable: true
-  })
-  emailDomain!: string
-
-  @Column({
-    name: 'use_xid_whitelist',
-    type: 'boolean',
-    default: false,
-    nullable: true
-  })
-  useXidWhitelist!: boolean
-
-  @Column({ name: 'owner', type: 'integer', nullable: true })
-  owner!: number
-
-  @Column({ name: 'org_id', type: 'integer', nullable: true })
-  orgId!: number
-
-  @Column({ name: 'context', type: 'varchar', length: 1000, nullable: true })
+  @Column({ type: 'varchar', length: 1000, nullable: true })
   context!: string
-
-  @Column({ name: 'course_id', type: 'integer', nullable: true })
-  courseId!: number
 
   @Column({
     name: 'owner_sees_participation_stats',
@@ -179,34 +98,252 @@ export class Conversation {
   })
   ownerSeesParticipationStats!: boolean
 
-  @Column({ name: 'auth_needed_to_vote', type: 'boolean', nullable: true })
-  authNeededToVote!: boolean
-
-  @Column({ name: 'auth_needed_to_write', type: 'boolean', nullable: true })
-  authNeededToWrite!: boolean
-
-  @Column({ name: 'auth_opt_fb', type: 'boolean', nullable: true })
-  authOptFb!: boolean
-
-  @Column({ name: 'auth_opt_tw', type: 'boolean', nullable: true })
-  authOptTw!: boolean
-
-  @Column({ name: 'auth_opt_allow_3rdparty', type: 'boolean', nullable: true })
-  authOptAllow3rdparty!: boolean
+  @Column({ name: 'course_id', type: 'integer', nullable: true })
+  courseId!: number
 
   @Column({
-    name: 'modified',
+    name: 'lti_users_only',
+    type: 'boolean',
+    nullable: true,
+    default: false
+  })
+  ltiUsersOnly!: boolean
+
+  @Column({ name: 'link_url', type: 'varchar', length: 9999, nullable: true })
+  linkUrl!: string
+
+  @Column({ type: 'integer', default: 1 })
+  upvotes!: number
+
+  @Column({ name: 'parent_url', type: 'varchar', length: 9999, nullable: true })
+  parentUrl!: string
+
+  @Column({ name: 'vis_type', type: 'integer', default: 0 })
+  visType!: number
+
+  @Column({ name: 'write_type', type: 'integer', default: 1 })
+  writeType!: number
+
+  @Column({ type: 'varchar', length: 20, nullable: true })
+  bgcolor!: string
+
+  @Column({ name: 'help_type', type: 'integer', default: 1 })
+  helpType!: number
+
+  @Column({
+    name: 'socialbtn_type',
+    type: 'integer',
+    default: 0
+  })
+  socialbtnType!: number
+
+  @Column({ name: 'style_btn', type: 'varchar', length: 500, nullable: true })
+  styleBtn!: string
+
+  @Column({
+    name: 'auth_needed_to_vote',
+    type: 'boolean',
+    nullable: true,
+    default: false
+  })
+  authNeededToVote!: boolean
+
+  @Column({
+    name: 'auth_needed_to_write',
+    type: 'boolean',
+    nullable: true,
+    default: true
+  })
+  authNeededToWrite!: boolean
+
+  @Column({
+    name: 'auth_opt_fb',
+    type: 'boolean',
+    nullable: true,
+    default: true
+  })
+  authOptFb!: boolean
+
+  @Column({
+    name: 'auth_opt_tw',
+    type: 'boolean',
+    nullable: true,
+    default: true
+  })
+  authOptTw!: boolean
+
+  @Column({
+    name: 'auth_opt_allow_3rdparty',
+    type: 'boolean',
+    nullable: true,
+    default: true
+  })
+  authOptAllow3rdparty!: boolean
+
+  @Column({ name: 'help_bgcolor', type: 'varchar', length: 20, nullable: true })
+  helpBgcolor!: string
+
+  @Column({ name: 'help_color', type: 'varchar', length: 20, nullable: true })
+  helpColor!: string
+
+  @Column({
+    name: 'is_data_open',
+    type: 'boolean',
+    default: false,
+    nullable: true
+  })
+  isDataOpen!: boolean
+
+  @Column({
+    name: 'is_slack',
+    type: 'boolean',
+    default: false,
+    nullable: true
+  })
+  isSlack!: boolean
+
+  @Column({
+    name: 'is_curated',
+    type: 'boolean',
+    default: false,
+    nullable: true
+  })
+  isCurated!: boolean
+
+  @Column({
+    name: 'dataset_explanation',
+    type: 'varchar',
+    length: 50000,
+    nullable: true
+  })
+  datasetExplanation!: string
+
+  @Column({
+    name: 'write_hint_type',
+    type: 'integer',
+    default: 1
+  })
+  writeHintType!: number
+
+  @Column({
+    name: 'subscribe_type',
+    type: 'integer',
+    default: 1
+  })
+  subscribeType!: number
+
+  @Column({ name: 'org_id', type: 'integer', nullable: true })
+  orgId!: number
+
+  @Column({
+    name: 'need_suzinvite',
+    type: 'boolean',
+    default: false,
+    nullable: true
+  })
+  needSuzinvite!: boolean
+
+  @Column({
+    name: 'use_xid_whitelist',
+    type: 'boolean',
+    default: false,
+    nullable: true
+  })
+  useXidWhitelist!: boolean
+
+  @Column({
+    name: 'prioritize_seed',
+    type: 'boolean',
+    default: false,
+    nullable: true
+  })
+  prioritizeSeed!: boolean
+
+  @Column({
+    type: 'bigint',
+    default: () => 'now_as_millis()',
+    nullable: true
+  })
+  created!: number
+
+  @Column({
     type: 'bigint',
     default: () => 'now_as_millis()',
     nullable: true
   })
   modified!: number
 
-  @Column({
-    name: 'created',
-    type: 'bigint',
-    default: () => 'now_as_millis()',
-    nullable: true
-  })
-  created!: number
+  // @ManyToOne(() => Course)
+  // @JoinColumn({ name: 'course_id' })
+  // course!: Course
+
+  @ManyToOne(() => User)
+  @JoinColumn({ name: 'org_id' })
+  orgUser!: User
+
+  @ManyToOne(() => User)
+  @JoinColumn({ name: 'owner' })
+  ownerUser!: User
 }
+
+/*
+ *** COLUMN SCHEMA ***
+  column_name,data_type,is_nullable,character_maximum_length,column_default
+  zid,integer,NO,,nextval('conversations_zid_seq'::regclass)
+  topic,character varying,YES,1000,
+  description,character varying,YES,50000,
+  is_anon,boolean,YES,,true
+  is_active,boolean,YES,,false
+  is_draft,boolean,YES,,false
+  is_public,boolean,YES,,true
+  email_domain,character varying,YES,200,
+  owner,integer,YES,,
+  participant_count,integer,YES,,"0"
+  created,bigint,YES,,now_as_millis()
+  strict_moderation,boolean,YES,,false
+  profanity_filter,boolean,YES,,true
+  spam_filter,boolean,YES,,true
+  context,character varying,YES,1000,
+  modified,bigint,YES,,now_as_millis()
+  owner_sees_participation_stats,boolean,YES,,false
+  course_id,integer,YES,,
+  lti_users_only,boolean,YES,,false
+  link_url,character varying,YES,9999,
+  upvotes,integer,NO,,1
+  parent_url,character varying,YES,9999,
+  vis_type,integer,NO,,"0"
+  write_type,integer,NO,,1
+  bgcolor,character varying,YES,20,
+  help_type,integer,NO,,1
+  socialbtn_type,integer,NO,,"0"
+  style_btn,character varying,YES,500,
+  auth_needed_to_vote,boolean,YES,,false
+  auth_needed_to_write,boolean,YES,,true
+  auth_opt_fb,boolean,YES,,true
+  auth_opt_tw,boolean,YES,,true
+  auth_opt_allow_3rdparty,boolean,YES,,true
+  help_bgcolor,character varying,YES,20,
+  help_color,character varying,YES,20,
+  is_data_open,boolean,YES,,false
+  is_slack,boolean,YES,,false
+  is_curated,boolean,YES,,false
+  dataset_explanation,character varying,YES,50000,
+  write_hint_type,integer,NO,,1
+  subscribe_type,integer,NO,,1
+  org_id,integer,YES,,
+  need_suzinvite,boolean,YES,,false
+  use_xid_whitelist,boolean,YES,,false
+  prioritize_seed,boolean,YES,,false
+
+  *** INDEXES ***
+  index_name,index_definition
+  conversations_zid_key,CREATE UNIQUE INDEX conversations_zid_key ON public.conversations USING btree (zid)
+  conversations_owner_idx,CREATE INDEX conversations_owner_idx ON public.conversations USING btree (owner)
+  conversations_zid_index,CREATE UNIQUE INDEX conversations_zid_index ON public.conversations USING btree (zid)
+
+  *** FOREIGN KEYS ***
+  constraint_name,column_name,foreign_table_name,foreign_column_name
+  conversations_owner_fkey,owner,users,uid
+  conversations_course_id_fkey,course_id,courses,course_id
+  conversations_actual_ownr_fkey,org_id,users,uid
+*/
--- src/entities/User.ts
+++ src/entities/User.ts
@@ -1,53 +1,90 @@
-import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm'
+import { Column, Entity, Index, PrimaryGeneratedColumn } from 'typeorm'
 
 @Entity('users')
+@Index('email_unique_check', ['email'], { unique: true })
+@Index('users_uid_key', ['uid'], { unique: true })
+@Index('site_domain_whitelist_idx', ['site_id'])
 export class User {
-  @PrimaryGeneratedColumn({ name: 'uid' })
+  @PrimaryGeneratedColumn()
   uid!: number
 
-  @Column({ name: 'hname', type: 'varchar', length: 746, nullable: true })
+  @Column({ type: 'varchar', length: 746, nullable: true })
   hname!: string
 
-  @Column({ name: 'username', type: 'varchar', length: 128, nullable: true })
+  @Column({ type: 'varchar', length: 128, nullable: true })
+  pwhash!: string
+
+  @Column({ type: 'varchar', length: 128, nullable: true })
   username!: string
 
-  @Column({ name: 'email', type: 'varchar', length: 256, nullable: true })
+  @Column({ type: 'varchar', length: 256, nullable: true })
   email!: string
 
-  @Column({ name: 'is_owner', type: 'boolean', default: false, nullable: true })
+  @Column({ name: 'is_owner', type: 'boolean', nullable: true, default: false })
   isOwner!: boolean
 
-  @Column({ name: 'zinvite', type: 'varchar', length: 300, nullable: true })
+  @Column({ type: 'varchar', length: 300, nullable: true })
   zinvite!: string
 
-  @Column({ name: 'oinvite', type: 'varchar', length: 300, nullable: true })
+  @Column({ type: 'varchar', length: 300, nullable: true })
   oinvite!: string
 
-  @Column({ name: 'tut', type: 'smallint', default: 0, nullable: true })
+  @Column({ type: 'smallint', nullable: true, default: 0 })
+  plan!: number
+
+  @Column({ type: 'smallint', nullable: true, default: 0 })
   tut!: number
 
   @Column({
     name: 'site_id',
     type: 'varchar',
     length: 256,
-    default: () => 'random_polis_site_id()',
-    nullable: false
+    default: () => 'random_polis_site_id()'
   })
   siteId!: string
 
   @Column({
     name: 'site_owner',
     type: 'boolean',
-    default: true,
-    nullable: false
+    default: true
   })
   siteOwner!: boolean
 
+  @Column({ type: 'boolean', default: false })
+  test!: boolean
+
   @Column({
-    name: 'created',
     type: 'bigint',
-    default: () => 'now_as_millis()',
-    nullable: true
+    nullable: true,
+    default: () => 'now_as_millis()'
   })
   created!: number
 }
+
+/*
+  *** COLUMN SCHEMA ***
+  column_name,data_type,is_nullable,character_maximum_length,column_default
+  uid,integer,NO,,nextval('users_uid_seq'::regclass)
+  hname,character varying,YES,746,
+  pwhash,character varying,YES,128,
+  username,character varying,YES,128,
+  email,character varying,YES,256,
+  is_owner,boolean,YES,,false
+  zinvite,character varying,YES,300,
+  oinvite,character varying,YES,300,
+  created,bigint,YES,,now_as_millis()
+  plan,smallint,YES,,"0"
+  tut,smallint,YES,,"0"
+  site_id,character varying,NO,256,random_polis_site_id()
+  site_owner,boolean,YES,,true
+  test,boolean,YES,,false
+
+  *** INDEXES ***
+  index_name,index_definition
+  email_unique_check,CREATE UNIQUE INDEX email_unique_check ON public.users USING btree (email)
+  users_uid_key,CREATE UNIQUE INDEX users_uid_key ON public.users USING btree (uid)
+  site_domain_whitelist_idx,CREATE INDEX site_domain_whitelist_idx ON public.users USING btree (site_id)
+
+  *** FOREIGN KEYS ***
+  none
+*/
